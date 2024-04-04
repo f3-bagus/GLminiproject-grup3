b71cc6f51932d2fb182b74d939b5bf3292a17bbc
Afifah Naura Kamilia <afifahnaurak@gmail.com>
2024-04-04 14:54:28 +0800

    feat(landing-page): implement form submission with error handling and success message in contact us section

--- script.js
+++ script.js
@@ -26,4 +26,32 @@ $(document).ready(function () {
 
     $("#productModal").modal("show");
   });
+
+  $(".message-form").submit(function(event) {
+    var form = $(this);
+    var formData = form.serialize();
+    $.ajax({
+      type: form.attr('method'),
+      url: form.attr('action'),
+      data: formData,
+      success: function(response) {
+        swal({
+          title: "Success!",
+          text: "Your message has been sent successfully!",
+          icon: "success",
+          button: "OK",
+        });
+        form[0].reset();
+      },
+      error: function(xhr, status, error) {
+        swal({
+          title: "Error!",
+          text: "An error occurred while sending your message : " + error,
+          icon: "error",
+          button: "OK",
+        });
+      }
+    });
+    event.preventDefault();
+  });
 });
